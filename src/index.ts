1dea848268d1def6eba4f0095f4c28276373350e
Ana Elisa Scanavacca Santos <scaninha12@outlook.com>
2026-03-25 10:18:52 -0400

    Banco de Dados

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import mysql from 'mysql2/promise';
 import express from 'express'
 const app = express()
+app.use(express.json())
 const connection = mysql.createPool({
     host: 'localhost',
     user: 'root',
@@ -21,6 +22,15 @@ app.post("/pessoas", async(req, res) => {
     try {
         //const preparacao = await connection.prepare("select * from pessoa");
          const {id,nome} = req.body
+         //valide se o id e o nome foram passados corretamente. ( algum valor)
+         // se não foram, retorne o código 400 com a mensagem "id ou nome invalidos"
+        //não deixe o código executar a parte de baixo quando for inválido.
+        if(!id || !nome) {
+            res.status(400).json({mensagem:" id ou nome inválidos"});
+            return;
+        }
+
+
         const [resultado, campos] =
             await connection.execute(`insert into pessoa values (?,?)`, [id, nome])
         console.log(resultado)
