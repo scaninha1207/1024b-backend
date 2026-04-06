f42e72b1460dc101828b30b24fb05c9889a82ebc
Ana Elisa Scanavacca Santos <scaninha12@outlook.com>
2026-04-06 16:47:50 -0400

    Atividade

--- src/index.ts
+++ src/index.ts
@@ -127,65 +127,24 @@ try {
  * Crie uma rota chamada `listar_produtos_caros` que retorne os produtos
  * que custem mais de R$: 100,00
  * 
- */ 
-
-
-app.get("/cadastro_produto", async (req, res) => {
-  try {
-
-    const [resultado, campos]
-      = await connection.execute(`SELECT * FROM produto`)
-    console.log(resultado)
-    res.status(200).json(resultado)
-  } catch (err) {
-    console.log(err);
-
-    if (err instanceof Error && 'code' in err && err.code === 'ECONNREFUSED') {
-      res.status(500).json({ mensagem: "Erro: Ligue o LARAGON!!!" })
-    }
-    else if (err instanceof Error && 'code' in err && err.code === 'ER_BAD_DB_ERROR') {
-      res.status(500).json({ mensagem: "Erro: Crie o banco de dados ou confira se o nome está correto!" })
-    } else if (err instanceof Error && 'code' in err && err.code === 'ER_ACCESS_DENIED_ERROR') {
-      res.status(500).json({ mensagem: "Erro: Confira o seu USER e Senha de Conexão!" })
-    } else if (err instanceof Error && 'code' in err && err.code === 'ER_NO_SUCH_TABLE') {
-      res.status(500).json({ mensagem: "Erro: Confira o nome de sua tabela ou crie a tabela!" })
-    } else if (err instanceof Error && 'code' in err && err.code === 'ER_PARSE_ERROR') {
-      res.status(500).json({ mensagem: "Erro: Confira o código SQL do EXECUTE!" })
-    } else {
-
-
-      res.status(500).json({ mensagem: "Erro no servidor!" })
-    }
-  }
-}
-)
-
-
-
-
-
-
-
-
-
-
+ */
 
 
 app.post("/cadastro_produto", async (req, res) => {
   try {
     // "execute" irá chamar internamente a preparação e a consulta (query)
     // const preparacao = await connection.prepare("select * from pessoa");
-    const { id, nome,categoria,preco,data_criacao,data_modificacao } = req.body
+    const { id, nome, categoria, preco, data_criacao, data_modificacao } = req.body
     //Valide se o id e o nome foram passados corretamente. (Algum valor)
     //Se não foram, retone o código 400 com a mensagem "id ou nome inválidos"
     //Não deixe o código executar a parte de baixo quando for inválido.
 
-    if (id != null && nome != "" && categoria != null && preco != null && data_criacao !=null && data_modificacao != null) {
-      res.status(400).json({ mensagem: "Id, nome, categoria,preço,data de criação e data de modificação são obrigatórios" })
+    if (id == null || nome == "" || categoria == null || preco == null || data_criacao == null || data_modificacao == null) {
+      return res.status(400).json({ mensagem: "Id, nome, categoria,preço,data de criação e data de modificação são obrigatórios" })
     }
 
     const [resultado, campos]
-      = await connection.execute(`insert into pessoa value (?,?,?,?,?,?)`, [id, nome,categoria,preco,data_criacao,data_modificacao])
+      = await connection.execute(`insert into pessoa value (?,?,?,?,?,?)`, [id, nome, categoria, preco, data_criacao, data_modificacao])
     res.status(201).json({ mesagem: "Sucesso" })
     console.log(resultado)
   } catch (err) {
@@ -212,6 +171,35 @@ app.post("/cadastro_produto", async (req, res) => {
 
 
 
+app.get("/listar_produto", async (req, res) => {
+  try {
+    const [resultado] = await connection.execute(`SELECT * FROM produto`);
+    res.status(200).json(resultado);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ mensagem: "Erro no servidor!" });
+  }
+});
 
 
 
+app.get("/listar_produtos_informatica", async (req, res) => {
+  try {
+    const [resultado] = await connection.execute(`SELECT * FROM produto WHERE categoria = 'informatica'`);
+    res.status(200).json(resultado);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ mensagem: "Erro no servidor!" });
+  }
+});
+
+
+app.get("/listar_produtos_caros", async (req, res) => {
+  try {
+    const [resultado] = await connection.execute(`SELECT * FROM produto WHERE preco > ?`, [100.00]);
+    res.status(200).json(resultado);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ mensagem: "Erro no servidor!" });
+  }
+});
