827d90d8c9c3e924c9eabf3fe2eab99170de1569
Ana Elisa Scanavacca Santos <scaninha12@outlook.com>
2026-04-29 10:18:49 -0400

    exercicio

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import mysql from 'mysql2/promise';
+import mysql, { type RowDataPacket } from 'mysql2/promise';
 
 import express from 'express'
 import MysqlErrorHandle from './mysql_error_handle.js';
@@ -11,7 +11,9 @@ app.use(express.json())
  app.listen(8000, () => {
    console.log("Servidor iniciado na porta 8000")})
 
-
+interface IQuantidadePedido extends RowDataPacket{
+  quantidade_pedidos:number
+}
 
 
 
@@ -49,7 +51,7 @@ app.get("\pedidos_2026", async (req, res) => {
     const [resultado] = await connection.execute(`SELECT clientes.nome, pedidos.datapedido
 FROM dbteremercado.clientes 
 INNER JOIN dbteremercado.pedidos
-ON clientes.idclientes = pedidos.clientes_idclientes;`);
+ON clientes.idclientes = pedidos.clientes_idclientes WHERE datapedido >= '2026-01-01' AND datapedido <='2026-12-31;`);
     res.status(200).json(resultado);
   } catch (err) {
     
@@ -70,9 +72,9 @@ mysqlErrorHandle.validar()
 //USE O COMANDO COUNT(*) para contar as quantidades.
 app.get("/quantidade_pedidos", async (req, res) => {
   try {
-    const [resultado] = await connection.execute(`SELECT SUM(quantidade) AS quantidade_pedidos
-FROM itenspedidos;`);
-    res.status(200).json(resultado);
+    const [resultado] = await connection.execute<IQuantidadePedido[]>(`SELECT COUNT(*) AS quantidade_pedidos FROM pedidos;`);
+    const [quantidadePedidos] = [...resultado]
+    res.status(200).json(quantidadePedidos);
   } catch (err) {
     
  const mysqlErrorHandle = new MysqlErrorHandle(err,res)
@@ -87,14 +89,33 @@ mysqlErrorHandle.validar()
 //4
 //Crie uma rota chamada '/quantidade_pedidos_clientes' que retorne um json no formato '[{nome:"tere,quantidade_pedidos:1000"}]' que retorne todos os clientes e a quantidade 
 //de pedidos que cada cliente fez.
-
 app.get("/quantidade_pedidos_clientes", async (req, res) => {
   try {
-    const [resultado] = await connection.execute(`SELECT clientes.nome AS nome,  
-      itenspedidos.quantidade AS quantidade_pedidos 
-FROM dbteremercado.clientes 
-INNER JOIN dbteremercado.itenspedidos
-ON clientes.idclientes = itenspedidos.pedidos_idpedidos`);
+    const [resultado] = await connection.execute(`SELECT clientes.nome as nome, SUM(quantidade) AS quantidade_pedidos FROM clientes 
+      INNER JOIN pedidos ON clientes.idclientes = pedidos.clientes_idclientes`);
+    res.status(200).json(resultado);
+  } catch (err) {
+    
+ const mysqlErrorHandle = new MysqlErrorHandle(err,res)
+mysqlErrorHandle.validar()
+
+}
+});
+
+
+//5) quantidade_produtos_por_cliente .Crie um código que retorne o nome do cliente e a quantidade de produtos que cada pedido tem formato [{nome:"Nome Cliente",idpedido:1,quantidade_produtos:1000}]
+app.get("/quantidade_produtos_por_cliente", async (req, res) => {
+  try {
+    const [resultado] = await connection.execute(` SELECT 
+        clientes.nome AS nome, pedidos.idpedidos,
+        SUM(quantidade) AS quantidade_produtos FROM clientes
+INNER JOIN pedidos 
+ON clientes.idcliente = pedidos.idcliente
+INNER JOIN itenspedidos 
+ON pedidos.idpedidos = itenspedidos.idpedidos
+GROUP BY clientes.nome, pedidos.idpedidos`
+    
+    );
     res.status(200).json(resultado);
   } catch (err) {
     
@@ -108,6 +129,27 @@ mysqlErrorHandle.validar()
 
 
 
+
+
+//6) valor_pedido_total .Crie um código que retorne o nome do clientee o valor total de cada pedido [{nome:"Nome Cliente", valor_total:1000}]
+app.get("/valor_pedido_total", async (req, res) => {
+  try {
+    const [resultado] = await connection.execute(`SELECT 
+  clientes.nome,
+  SUM(produtos.preco) AS valor_total
+FROM clientes
+JOIN pedidos ON pedidos.cliente_id = clientes.idclientes
+JOIN itens_pedido ON itens_pedido.pedido_id = pedidos.id
+JOIN produtos ON produtos.idprodutos = itens_pedido.produto_id
+GROUP BY clientes.idclientes, clientes.nome` );
+    res.status(200).json(resultado);
+  } catch (err) {
+    
+ const mysqlErrorHandle = new MysqlErrorHandle(err,res)
+mysqlErrorHandle.validar()
+
+}
+});
 
 
 
